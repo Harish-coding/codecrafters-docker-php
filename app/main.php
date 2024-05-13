40ba92ab871cf2516517b0f24331f15b1bec8834
Harish Saravanan Anuja <>
2024-05-13 19:56:19 +0300

    docker image

--- app/main.php
+++ app/main.php
@@ -20,6 +20,122 @@ chroot("./temporary");
 $pid = pcntl_unshare(CLONE_NEWPID);
 
 
+// Function to get Docker auth token
+function get_docker_token($image_name) {
+    // You need to get an auth token, but you don't need a username/password
+    // Say your image is busybox/latest, you would make a GET request to this
+    // URL: https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/busybox:pull
+    $url = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/{$image_name}:pull";
+    
+    // Initialize cURL session
+    $ch = curl_init();
+    
+    // Set cURL options
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+    // Execute the request
+    $res = curl_exec($ch);
+    
+    // Check for errors
+    if ($res === false) {
+        echo "cURL error: " . curl_error($ch);
+        return null;
+    }
+    
+    // Close cURL session
+    curl_close($ch);
+    
+    // Decode JSON response
+    $res_json = json_decode($res, true);
+    
+    // Return the token
+    return $res_json["token"];
+}
+
+function build_docker_headers($token) {
+  return [
+      "Accept: application/vnd.docker.distribution.manifest.v2+json",
+      "Authorization: Bearer $token",
+  ];
+}
+
+function get_docker_image_manifest($headers, $image_name) {
+  $manifest_url = "https://registry.hub.docker.com/v2/library/{$image_name}/manifests/latest";
+  
+  // Initialize cURL session
+  $ch = curl_init();
+  
+  // Set cURL options
+  curl_setopt($ch, CURLOPT_URL, $manifest_url);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  
+  // Execute the request
+  $res = curl_exec($ch);
+  
+  // Check for errors
+  if ($res === false) {
+      echo "cURL error: " . curl_error($ch);
+      return null;
+  }
+  
+  // Close cURL session
+  curl_close($ch);
+  
+  // Decode JSON response
+  $res_json = json_decode($res, true);
+  
+  // Return the manifest
+  return $res_json;
+}
+
+function download_image_layers($headers, $image, $layers) {
+  // Create a temporary directory
+  $dir_path = sys_get_temp_dir() . '/' . uniqid('docker_image_');
+  mkdir($dir_path);
+
+  // Loop through the layers to download and extract each one
+  foreach ($layers as $layer) {
+      $url = "https://registry.hub.docker.com/v2/library/{$image}/blobs/{$layer['digest']}";
+      fwrite(STDERR, $url);
+      
+      // Initialize cURL session
+      $ch = curl_init();
+      
+      // Set cURL options
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      
+      // Execute the request
+      $res = curl_exec($ch);
+      
+      // Check for errors
+      if ($res === false) {
+          echo "cURL error: " . curl_error($ch);
+          return null;
+      }
+      
+      // Close cURL session
+      curl_close($ch);
+      
+      // Write the response to a temporary file
+      $tmp_file = $dir_path . "/manifest.tar";
+      file_put_contents($tmp_file, $res);
+      
+      // Extract the contents of the tar file
+      $tar = new PharData($tmp_file);
+      $tar->extractTo($dir_path);
+      
+      // Remove the temporary file
+      unlink($tmp_file);
+  }
+  
+  return $dir_path;
+}
+
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // echo "Logs from your program will appear here!\n";
 
@@ -63,8 +179,26 @@ else {
     }
     
   } else if (str_replace('\n', '', $argv[4]) === 'mypid') {
+    // mydocker run alpine:latest /usr/local/bin/docker-explorer mypid
     echo getmypid() . PHP_EOL;
+  } else if (str_replace('\n', '', $argv[4]) === 'hey') {
+    // mydocker run alpine:latest /bin/echo hey
+    $image_name = $argv[2]; // Replace with your desired image name
+    $token = get_docker_token($image_name);
+    $header = build_docker_headers($token);
+    $manifest = get_docker_image_manifest($header, $image_name);
+    $dir_path = download_image_layers($header, $image_name, $manifest["layers"]);
+
+      
+    $completed_process = shell_exec("unshare -fpu chroot $dir_path $command " . implode(' ', $args));
+    $return_code = shell_exec("echo $?");
+
+    echo $completed_process;
+    echo $return_code;
+    exit($return_code);
+
   }
+
 
 }
 
